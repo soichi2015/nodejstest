40aa9c69c373711afce9d48598f142247abfa4ee
soichi2015 <soichi@r6.dion.ne.jp>
2015-11-06 14:10:29 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+    var http = require('http');
+     
+    http.createServer(function (request, response) {
+      response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.end('Hello World\n');
+    }).listen(8124);
+     
+    console.log('Server running at http://127.0.0.1:8124/');
